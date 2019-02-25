a81234801bf95f38aba7543a50f68d52c63f0740
Zhu Zheng <zhuzheng728@gmail.com>
2019-02-25 03:00:36 +0000

    add support for cd and pwd

--- hw1/shell.c
+++ hw1/shell.c
@@ -9,6 +9,7 @@
 #include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
+#include <limits.h>
 
 #include "tokenizer.h"
 
@@ -29,6 +30,8 @@ pid_t shell_pgid;
 
 int cmd_exit(struct tokens *tokens);
 int cmd_help(struct tokens *tokens);
+int cmd_pwd(struct tokens *tokens);
+int cmd_cd(struct tokens *tokens);
 
 /* Built-in command functions take token array (see parse.h) and return int */
 typedef int cmd_fun_t(struct tokens *tokens);
@@ -43,6 +46,8 @@ typedef struct fun_desc {
 fun_desc_t cmd_table[] = {
   {cmd_help, "?", "show this help menu"},
   {cmd_exit, "exit", "exit the command shell"},
+  {cmd_pwd, "pwd", "print the current working directory"},
+  {cmd_cd, "cd", "change the current working directory"},
 };
 
 /* Prints a helpful description for the given command */
@@ -55,6 +60,37 @@ int cmd_help(unused struct tokens *tokens) {
 /* Exits this shell */
 int cmd_exit(unused struct tokens *tokens) {
   exit(0);
+}
+
+/* Prints the current working directory */
+int cmd_pwd(unused struct tokens *tokens) {
+  char cwd[PATH_MAX];
+  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    printf("%s\n", cwd);
+  } else {
+    perror("getcwd() error");
+    return 0;
+  }
+  return 1;
+}
+
+/* Changes the current working directory to the directory which the argument indicate */
+int cmd_cd(unused struct tokens *tokens) {
+  char *default_dir = "/home/vagrant";
+  size_t length = tokens_get_length(tokens);
+  char *dir; 
+  if (length == 1) {
+    dir = default_dir;
+  } else {
+    dir = tokens_get_token(tokens, 1);
+  }
+  if (chdir(dir) == 0) {
+    printf("%s\n", dir);
+  } else {
+    perror("chdir() error");
+    return 0;
+  }
+  return 1;
 }
 
 /* Looks up the built-in command, if it exists. */
